b9d73a321845e13fe97187bd8a599465f0330597
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-06-29 15:00:30 +0700

    fix: update toast className in FavoriteButton for consistent styling (from w-100 to w-auto)

--- src/components/ui/app-ui/FavoriteButton.tsx
+++ src/components/ui/app-ui/FavoriteButton.tsx
@@ -25,14 +25,14 @@ const FavoriteButton = ({ data, className, ...props }: FavoriteButtonProps) => {
         position: 'top-center',
         icon: <Trash2 className='size-5' />,
         duration: 2000,
-        className: '!w-100 !flex !justify-center !items-center',
+        className: '!w-auto !flex !justify-center !items-center',
       });
     } else {
       addFavorite(data);
       toast.success(`${data.title} Added to Favorites`, {
         position: 'top-center',
         duration: 2000,
-        className: '!w-100 !flex !justify-center !items-center',
+        className: '!w-auto !flex !justify-center !items-center',
       });
     }
   }
